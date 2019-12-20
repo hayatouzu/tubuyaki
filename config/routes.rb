56ff2b40e9359c5f818737a0bf556e098c7288d4
hayatouzu <kira.sri779@gmail.com>
2019-12-20 20:44:10 +0900

    create-controller-model　rootでview見れる

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'tweets#index'
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
 end
